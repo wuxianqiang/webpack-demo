c1b0edcb45a71ed583fc0cd5739e7475727e41cf
wuxianqiang <2631640352@qq.com>
2018-12-02 13:06:08 +0800

    生成HTML文件

--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  devServer: {
+    // 静态文件根目录
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    host: 'localhost'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'bundle.html'
+    })
+  ]
 }

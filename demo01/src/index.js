aa73e0caf0903fd3cd741bd88b1f31ce2ca9b75b
wupengli@58ganji.com <wupengli@58ganji.com>
2018-12-02 17:29:17 +0800

    配置loader了

--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,3 +1,6 @@
 import { name } from './name'
+import './base.css'
+
+document.querySelector('#root').innerHTML = name
 
 console.log(name)
--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -9,14 +9,25 @@ module.exports = {
   },
   devServer: {
     // 静态文件根目录
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: './dist',
     port: 8080,
     host: 'localhost'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // css-loader 处理CSS引入路径
+        // style-loader 把CSS转换为JS代码，在执行的时候注入一个style标签
+        // 可以用use也可以用loader
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'bundle.html'
+      filename: 'index.html'
     })
   ]
 }

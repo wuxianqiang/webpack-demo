10e415f34eb4995e3005409af28be8c52bd90bc3
wupengli@58ganji.com <wupengli@58ganji.com>
2018-12-02 17:52:20 +0800

    引入图片

--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,6 +1,11 @@
 import { name } from './name'
 import './base.css'
+import avatar from './image/26681854.png'
 
 document.querySelector('#root').innerHTML = name
+const AVATAR_URL = avatar // 相对于输出目录的跟路径
+let img = new Image()
+img.src = AVATAR_URL
+document.body.appendChild(img)
 
 console.log(name)
--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -20,14 +20,37 @@ module.exports = {
         // css-loader 处理CSS引入路径
         // style-loader 把CSS转换为JS代码，在执行的时候注入一个style标签
         // 可以用use也可以用loader
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              insertAt: 'top' // style 标签默认插入到尾部，配置参数来插入到头部
+            }
+          },
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(gif|jpg|jpej|png|bmp|eot|woff|woff2|ttf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      minify: {
+        removeAttributeQuotes: true // 去除双引号
+      },
+      hash: true // 防止缓存的
     })
   ]
 }

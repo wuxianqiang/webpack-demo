6d2deea76934b778f8b73b950e8398cc6dea42c2
wupengli@58ganji.com <wupengli@58ganji.com>
2018-12-02 18:27:34 +0800

    分离css

--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -21,8 +22,8 @@ module.exports = {
         // style-loader 把CSS转换为JS代码，在执行的时候注入一个style标签
         // 可以用use也可以用loader
         use: [
-          {
-            loader: 'style-loader',
+          { // 收集所有的CSS文件
+            loader: MiniCssExtractPlugin.loader, // css分离
             options: {
               insertAt: 'top' // style 标签默认插入到尾部，配置参数来插入到头部
             }
@@ -51,6 +52,9 @@ module.exports = {
         removeAttributeQuotes: true // 去除双引号
       },
       hash: true // 防止缓存的
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
     })
   ]
 }

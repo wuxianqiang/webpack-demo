1a76a6b07e6c67cb2260f30a9380bd2802d7dce4
wuxianqiang <2631640352@qq.com>
2019-01-27 09:50:49 +0800

    happypack多进程打包

--- demo03/src/index.js
+++ demo03/src/index.js
@@ -1,15 +1,1 @@
-// module.exports = 'hello world'
-
-// console.log('hello world')
-
-import './index.css'
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-
-function Home(params) {
-  return (
-    <div>Home</div>
-  )
-}
-
-ReactDOM.render(<Home/>, document.querySelector('#root'))
+console.log('hello world')
--- demo03/webpack.config.js
+++ demo03/webpack.config.js
@@ -1,54 +1,42 @@
 const path = require('path')
-const DllReferencePlugin = require('webpack/lib/DllReferencePlugin')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HappyPack = require('happypack')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve('dist'),
     filename: 'bundle.js',
-    // 当用 Webpack 去构建一个可以被其他模块导入使用的库时需要用到它们
-    // libraryTarget: 'var', // var=xxx
-    // libraryTarget: 'commonjs', exports['zfpx']=xxx
-    // libraryTarget: 'commonjs2', exports.exports = xxx
-    // libraryTarget: 'window', window.lib = xxx
-    // library: 'lib'
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
+    noParse: /jquery/, // 如果有require(jquery)就不解析
     rules: [
       {
-        test: /\.jsx?$/,
-        use: { // id为babel的进程处理
-          loader: 'happypack/loader?id=babel'
-        },
-        include: path.join(__dirname, 'src'),
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: { // id为css的进程处理
-          loader: 'happypack/loader?id=css'
-        },
-        include: path.join(__dirname, 'src'),
-        exclude: /node_modules/
+        // exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        test: /\.js$/,
+        use: 'happypack/loader?id=js'
       }
     ]
   },
   plugins: [
-    new DllReferencePlugin({
-      manifest: path.resolve(__dirname, 'dist/react.manifest.json')
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
     }),
-
     new HappyPack({
-      id: 'babel',
-      use: [{
-        loader: 'babel-loader'
-      }]
-    }),
-
-    new HappyPack({
-      id: 'css',
-      use: ['style-loader', 'css-loader']
+      id: 'js',
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      ]
     })
-  ]
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
 }
